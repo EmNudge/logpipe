c9577c91af6ed4cb7b8a5aac1123f939836a41e7
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-04-06 22:50:54 -0700

    reformat some stuff

--- out.js
+++ out.js
@@ -1,17 +1,24 @@
 let count = 0;
 const logRandom = () => {
   const random = Math.random();
-  if (random < .2) {
-    console.log('[INFO]', new Date(), '"log random"', count++);
-  } else if (random < .4) {
-    console.log('[WARN]', 'This is a dangerous error. You are fumbulating the fumblertrons. Stop immediately or the blimdpad will forbulmdip.');
-  } else if (random < .6) {
-    console.log('testing | here is some data: ', count++);
-  } else if (random < .8) {
-    console.log('my object', { a: 1, b: 2, c: 3 });
+  if (random < 0.2) {
+    console.log("[INFO]", new Date(), '"log random"', count++);
+  } else if (random < 0.4) {
+    console.log(
+      "[WARN]",
+      "This is a dangerous error. You are fumbulating the fumblertrons. Stop immediately or the blimdpad will forbulmdip."
+    );
+  } else if (random < 0.6) {
+    console.log("testing | here is some data: ", count++);
+  } else if (random < 0.8) {
+    console.log("my object", { 
+      a: 1,
+      b: 2,
+      c: Math.random() * 50
+    });
   } else {
-    console.log('server caught error!');
+    console.log("server caught error!");
   }
-}
+};
 
-setInterval(logRandom, 2000)
+setInterval(logRandom, 2000);
--- public/index.js
+++ public/index.js
@@ -3,9 +3,11 @@ import { $, $$, cloneTemplate, effect, highlightText, signal } from "./lib.js";
 const logsSig = signal(/**@type {string[]}*/ ([]));
 const filterSig = signal("");
 
-/** @param {HTMLElement} element */
-const applyVisibility = (element) => {
-  const filter = filterSig.value;
+/** 
+ * @param {HTMLElement} element 
+ * @param {string} filter
+ */
+const applyVisibility = (element, filter) => {
   if (element.textContent.includes(filter)) {
     element.style.display = "";
   } else {
@@ -13,32 +15,35 @@ const applyVisibility = (element) => {
   }
 };
 
-const logContainer = $(".container");
-effect(() => {
-  const logs = logsSig.value;
-  logContainer.innerHTML = "";
-  for (const log of logs) {
-    const logEl = cloneTemplate(".log");
-    logEl.innerHTML = highlightText(log);
-    applyVisibility(logEl);
-    logContainer.appendChild(logEl);
-  }
-});
 
-$("input.filter").addEventListener("input", (event) => {
-  filterSig.value = event.target.value;
-});
-effect(() => {
-  for (const logEl of $$(".container .log")) {
-    applyVisibility(logEl);
-  }
-});
+const logContainer = $(".container");
+/** @param {string} logStr */
+function appendLog(logStr) {
+  const logEl = cloneTemplate(".log");
+  logEl.innerHTML = highlightText(logStr);
+  applyVisibility(logEl, filterSig.value);
+  logContainer.prepend(logEl);
+}
+
+{ // apply filters
+  const filterInput = $("input.filter");
+  filterInput.addEventListener("input", (event) => {
+    const filter = event.target.value;
+    for (const logEl of $$(".container .log")) {
+      applyVisibility(logEl, filter);
+    }
+    filterSig.value = filter;
+  });
+}
 
 const cliSource = new EventSource("/cli-input");
 cliSource.onmessage = (event) => {
   const data = JSON.parse(event.data);
   if (Array.isArray(data)) {
     logsSig.value = [...logsSig.value, ...data];
+    for (const log of data) {
+      appendLog(log);
+    }
     return;
   }
   console.log("unknown data received", data);
