b471d5fc0f56d619946fa27c9ec5ea4460633b01
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-04-07 09:12:05 -0700

    update out for easier adding test cases

--- out.js
+++ out.js
@@ -22,23 +22,34 @@ function logRandom() {
     return;
   }
 
-  const random = Math.random();
-  if (random < 0.2) {
-    console.log("[INFO]", new Date(), '"log random"', count);
-  } else if (random < 0.4) {
-    console.log(
-      "[WARN]",
-      "This is a dangerous error. You are fumbulating the fumblertrons. Stop immediately or the blimdpad will forbulmdip."
-    );
-  } else if (random < 0.6) {
-    console.log("testing | here is some data: ", count);
-  } else if (random < 0.8) {
-    console.log("my object", {
-      a: 1,
-      b: 2,
-      c: Math.random() * 50,
-    });
-  } else {
-    console.log("server caught error!");
-  }
+  const branches = [
+    () => console.log("[INFO]", new Date(), '"log random"', count),
+    () =>
+      console.log(
+        "[WARN]",
+        "This is a dangerous error. You are fumbulating the fumblertrons. Stop immediately or the blimdpad will forbulmdip."
+      ),
+    () => console.log("testing | here is some data: ", count),
+    () =>
+      console.log("my object", {
+        a: 1,
+        b: 2,
+        c: Math.random() * 50,
+      }),
+    () =>
+      console.log(
+        "stringified",
+        JSON.stringify(
+          {
+            a: 1,
+            b: 2,
+            c: Math.random() * 50,
+          },
+          null,
+          2
+        )
+      ),
+    () => console.log("server caught error!"),
+  ];
+  branches[branches.length * Math.random() | 0]();
 }
