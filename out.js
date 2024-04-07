881dbab5ceb3d2c571a6eecc17dc5080ba0f7e30
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-04-06 20:15:57 -0700

    cleanup some code

--- out.js
+++ out.js
@@ -1,4 +1,17 @@
 let count = 0;
-setInterval(() => {
-  console.log('[INFO]', new Date(), '"log count"', count++);
-}, 2000)
+const logRandom = () => {
+  const random = Math.random();
+  if (random < .2) {
+    console.log('[INFO]', new Date(), '"log random"', count++);
+  } else if (random < .4) {
+    console.log('[WARN]', 'This is a dangerous error. You are fumbulating the fumblertrons. Stop immediately or the blimdpad will forbulmdip.');
+  } else if (random < .6) {
+    console.log('testing | here is some data: ', count++);
+  } else if (random < .8) {
+    console.log('my object', { a: 1, b: 2, c: 3 });
+  } else {
+    console.log('server caught error!');
+  }
+}
+
+setInterval(logRandom, 2000)
--- public/index.js
+++ public/index.js
@@ -1,24 +1,21 @@
 import { h, render } from "https://esm.sh/preact";
 import htm from "https://esm.sh/htm";
-import { signal, computed } from "https://esm.sh/@preact/signals";
+import { signal, effect } from "https://esm.sh/@preact/signals";
 import { highlightText } from "./lib.js";
 
 const html = htm.bind(h);
 
 const logsSig = signal([]);
 const filterSig = signal('');
-computed(() => console.log(logsSig.value));
-computed(() => console.log(filterSig.value));
+effect(() => console.log('filterSig', filterSig.value));
 
 function App() {
-  console.log(logsSig.value);
-
   return html`
     <input
       type="text"
       class="filter"
       placeholder="filter..."
-      onClick=${(e) => (filterSig.value = e.currentTarget.value)}
+      onInput=${(e) => (filterSig.value = e.currentTarget.value)}
     />
     ${filterSig.value}
     <div class="container" tab-index="0">
@@ -37,9 +34,7 @@ render(html`<${App} />`, document.querySelector("#app"));
 const cliSource = new EventSource("/cli-input");
 cliSource.onmessage = (event) => {
   const data = JSON.parse(event.data);
-  console.log("data received", data);
   if (Array.isArray(data)) {
-    console.log("array", data);
     logsSig.value = [...logsSig.value, ...data];
     return;
   }
