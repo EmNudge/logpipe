54d2e1e7e4a1122e957a0dde0006983cb2aff050
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-04-07 11:39:07 -0700

    make tags clickable

--- public/index.js
+++ public/index.js
@@ -1,13 +1,63 @@
-import { $, $$, cloneTemplate, effect, highlightText, isInView, signal } from "./lib.js";
+import {
+  $,
+  $$,
+  cloneTemplate,
+  effect,
+  highlightText,
+  isInView,
+  signal,
+} from "./lib.js";
 
 /** @typedef {{ input: string, date: number }} CliInput */
 
 const logsSig = signal(/**@type {CliInput[]}*/ ([]));
 const filterSig = signal("");
+const filterCountSig = signal(0);
+
+{
+  // set log counts
+  effect(() => {
+    $(".log-count .total").textContent = `(${logsSig.value.length})`;
+  });
+  effect(() => {
+    $(".log-count .filtered").textContent = filterSig.value.length
+      ? `filtered: (${filterCountSig.value})`
+      : "";
+  });
+}
+// update filtered items
+effect(() => {
+  const filter = filterSig.value;
+  let filterCount = 0;
+  for (const logEl of $$(".container .log")) {
+    const shouldDisplay = logEl.textContent.includes(filter);
+    if (shouldDisplay) filterCount++;
+    logEl.style.display = shouldDisplay ? "" : "none";
+  }
+  filterCountSig.value = filterCount;
+});
 
 const logContainer = $(".container");
 
 const tagsContainer = $(".tags");
+
+tagsContainer.addEventListener("click", (e) => {
+  const tagEl = e.target;
+  if (!(tagEl instanceof HTMLElement)) return;
+  if (tagEl.tagName !== "SL-BADGE") return;
+
+  const filterInput = /** @type {HTMLInputElement} */ ($(".filter"));
+  if (tagEl.getAttribute("variant") === "neutral") {
+    for (const tag of $$(".tags sl-badge")) {
+      tag.setAttribute("variant", "neutral");
+    }
+    tagEl.setAttribute("variant", "primary");
+    filterInput.value = filterSig.value = `[${tagEl.textContent}]`;
+  } else {
+    tagEl.setAttribute("variant", "neutral");
+    filterInput.value = filterSig.value = "";
+  }
+});
 /** @type {Set<string>} */
 const tagsSet = new Set();
 /** @param {string} text */
@@ -36,7 +86,10 @@ function getLogEl({ input, date }) {
       second: "numeric",
     })
   );
-  logEl.style.display = input.includes(filterSig.value) ? "" : "none";
+
+  const shouldDisplay = input.includes(filterSig.value);
+  logEl.style.display = shouldDisplay ? "" : "none";
+  if (shouldDisplay) filterCountSig.value++;
 
   return logEl;
 }
@@ -58,7 +111,7 @@ async function appendLog(...logEls) {
 {
   // apply filters
   const filterInput = /** @type {HTMLInputElement} */ ($(".filter"));
-  filterInput.addEventListener("input", (event) => {
+  filterInput.addEventListener("input", () => {
     const filter = filterInput.value;
     for (const logEl of $$(".container .log")) {
       logEl.style.display = logEl.textContent.includes(filter) ? "" : "none";
@@ -71,14 +124,12 @@ const cliSource = new EventSource("/cli-input");
 /** @param {Event & { data: string }} event */
 cliSource.onmessage = async (event) => {
   const data = JSON.parse(event.data);
-  
+
   if (Array.isArray(data)) {
     /** @type {CliInput[]} */
     const logs = data;
     logsSig.value = [...logsSig.value, ...logs];
     await appendLog(...logs.map((log) => getLogEl(log)));
-    $(".log-count").textContent = `(${logsSig.value.length})`;
-
     return;
   }
 
