bfbc3144a6ff35af1798246e84ccdf53c6d06d4c
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-04-12 16:35:16 -0700

    add type parsing

--- public/filter.js
+++ public/filter.js
@@ -7,16 +7,16 @@ const filterInputEl = /** @type {HTMLInputElement} */ ($("sl-input.filter"));
 let filterText = "";
 let filterItemsCount = 0;
 
-/** 
+/**
  * Applies filter to element based off of input content
- * 
+ *
  * @param {string} input
  * @param {HTMLElement} logEl */
 export const applyFilter = (input, logEl) => {
   const shouldDisplay = input.includes(filterText);
   logEl.style.display = shouldDisplay ? "" : "none";
   if (shouldDisplay) filterItemsCount++;
-}
+};
 
 /** @param {string} newText */
 export const setFilter = (newText) => {
@@ -24,11 +24,32 @@ export const setFilter = (newText) => {
 
   let filterCount = 0;
   const filter = filterText.toLowerCase();
-  for (const logEl of $$(".container .log")) {
-    const shouldDisplay = logEl.textContent.toLowerCase().includes(filter);
-    if (shouldDisplay) filterCount++;
-    logEl.style.display = shouldDisplay ? "" : "none";
+  if (/@`.+?`/.test(filter)) {
+    const tags = [...filter.matchAll(/@`(.+?)`/g)].map((m) => m[1]);
+    for (const logEl of $$(".container .log")) {
+      const shouldDisplay = tags
+        .map((tagGroup) => {
+          const elements = logEl.querySelectorAll(
+            tagGroup
+              .split(/\s*,\s*/)
+              .map((tag) => `span.${tag}`)
+              .join(",")
+          );
+          return elements.length;
+        })
+        .every((len) => len > 0);
+
+      if (shouldDisplay) filterCount++;
+      logEl.style.display = shouldDisplay ? "" : "none";
+    }
+  } else {
+    for (const logEl of $$(".container .log")) {
+      const shouldDisplay = logEl.textContent.toLowerCase().includes(filter);
+      if (shouldDisplay) filterCount++;
+      logEl.style.display = shouldDisplay ? "" : "none";
+    }
   }
+
   filterItemsCount = filterCount;
 
   // set filter log count text
@@ -45,8 +66,9 @@ filterInputEl.addEventListener("input", () => {
   setFilter(filter);
 });
 
-document.addEventListener('keydown', e => {
-  if (e.key !== '/' || e.metaKey || document.activeElement === filterInputEl) return;
+document.addEventListener("keydown", (e) => {
+  if (e.key !== "/" || e.metaKey || document.activeElement === filterInputEl)
+    return;
 
   filterInputEl.focus();
   e.preventDefault();
