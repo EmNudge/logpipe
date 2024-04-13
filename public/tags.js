a951b807de66e1a5df756bebad1b4c56b6c7f53e
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-04-12 19:25:06 -0700

    allow multi click

--- public/tags.js
+++ public/tags.js
@@ -3,23 +3,37 @@ import { $, $$, cloneTemplate } from "./lib.js";
 
 const tagsContainer = $(".tags");
 
+const setFilterForTags = () => {
+  const tagStrings = $$(".tags sl-badge")
+    .filter((el) => el.getAttribute("aria-pressed") === "true")
+    .map((el) => `tag="${el.textContent}"`);
+  
+  const tagGroup = tagStrings.length ? `@@${tagStrings.join(',')}` : '';
+  setFilter(tagGroup, true);
+};
+
 tagsContainer.addEventListener("click", (e) => {
   const tagEl = e.target;
   if (!(tagEl instanceof HTMLElement)) return;
   if (tagEl.tagName !== "SL-BADGE") return;
 
   if (tagEl.getAttribute("variant") === "neutral") {
-    for (const tag of $$(".tags sl-badge")) {
-      tag.setAttribute("variant", "neutral");
-      tag.setAttribute("aria-pressed", "false");
-    }
     tagEl.setAttribute("variant", "primary");
     tagEl.setAttribute("aria-pressed", "true");
-    setFilter(`@@tag="${tagEl.textContent}"`, true);
+    setFilterForTags();
   } else {
     tagEl.setAttribute("variant", "neutral");
-    tagEl.setAttribute("aria-pressed", "true");
-    setFilter("", true);
+    tagEl.setAttribute("aria-pressed", "false");
+    setFilterForTags();
+  }
+});
+
+const filterEl = $("sl-input.filter");
+filterEl.addEventListener("input", () => {
+  // reset tags
+  for (const tag of $$(".tags sl-badge")) {
+    tag.setAttribute("variant", "neutral");
+    tag.setAttribute("aria-pressed", "false");
   }
 });
 
